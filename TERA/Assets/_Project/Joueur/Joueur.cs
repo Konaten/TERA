8d494fa1332bba1834f9214c6cb7e640a76202d7
Kyutsune <jaipechofizz@gmail.com>
2026-01-13 12:35:26 +0100

    On ajoute les dégâts pris par le joueur
    (ceci implique le début du script de joueur)

--- TERA/Assets/_Project/Joueur/Joueur.cs
+++ TERA/Assets/_Project/Joueur/Joueur.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Joueur : MonoBehaviour
 {
+    private int pv = 100;
+
+    private AudioSource audioSource;
+    private AudioClip[] dmgClips;
+
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
 
+        // Charge tous les sons de dégâts depuis Resources/Joueur/DmgSound
+        dmgClips = Resources.LoadAll<AudioClip>("Joueur/DmgSound");
     }
 
     void Update()
     {
-
     }
 
+    public void PvRemoved(int dmgTaken)
+    {
+        pv -= dmgTaken;
+
+        if (dmgClips.Length > 0)
+        {
+            AudioClip clip = dmgClips[Random.Range(0, dmgClips.Length)];
+
+            audioSource.pitch = Random.Range(0.9f, 1.1f);
+            audioSource.PlayOneShot(clip);
+        }
+    }
 }
